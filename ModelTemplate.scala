330ef09d9841a61459ae2e0407c34a514f9bad23
Dimitris Tziatzios <dtzi@kth.se>
2018-09-22 13:51:31 +0200

    Updated Modified Model

--- ModelTemplate.scala
+++ ModelTemplate.scala
@@ -1,127 +1,115 @@
 package modbat
 import scala.util.Random;
-import java.util.Arrays;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
 
 import modbat.dsl._
 
 class ModelTemplate extends Model {
+    Class.forName("org.postgresql.Driver");
 
+    var con = DriverManager.getConnection("jdbc:postgresql://localhost:-/dbname","postgres", "User")
+    var databaseMetaData = con.getMetaData();//Get Column_Names.
 
-  def init_connect {
+    var table = ""//tablename
+    val random_columns = scala.util.Random//replace with modbat's built_in function  def chooseName(i: Int) = {
+    //parameterName(choose(0, i))}
 
+    val random_tables = scala.util.Random
+    var a = random_columns.nextInt(9)//table names
+    System.out.println(a)
+    var counter = 0
+    var col = 0
+    var colArray = Vector[Int]()//vector, remember the random_numbers to use them later.
 
+    def create_table{
 
-      Class.forName("org.postgresql.Driver");
+      for(x <- 1 to a){
 
-      var con = DriverManager.getConnection("jdbc:postgresql://localhost:/dbname","postgres", "user");
-      var databaseMetaData = con.getMetaData();//Get Column_Names and attributes.
-      var stat = con.createStatement();
-      //var counter = 0;
-
-
-
-      var table = "";
-      var a = 0; //use for number of tables.
-      //get random number of tables. a should be random.
-
-
-      //insert random number of columns PRIORITY.
-        for( a <- 0 to 10){
-          //var attrs = A(Random.nextInt(A.size))//pick a random attribute.
-
-          table = "table"+a;//name of the table, table0 table1 ...
-          //create table.
-          var createTable = con.createStatement ();
-            createTable.executeUpdate ("CREATE TABLE " + table
-                + "(id INTEGER )");
-
-                   //var columnNumber = 5; Number of columns, randomly choose number of columns.
-          val random_cols = scala.util.Random
-          var col = random_cols.nextInt(7) //get number of columns randomly.
-                  //DEBUG::
-          var b = 0;
-                   //Insert by altering table the number of columns.
-           for( b <- 1 to col){
-              var s1 = "ALTER TABLE "
-              var s2 = table;
-              var st = s1.concat(s2);//String Manipulation, need to change.TODO
-              var s3 = " add ";
-              var st2 = st.concat(s3);//String Manipulation, need to change.TODO
-              var stz = con.createStatement();
-              var columnName = "Column" +b;//Name of columns, column1 column2 ...
-              var alter_table = st2 + columnName + " varchar(30)";//Get data_type randomized with altering.
-              System.out.println(alter_table);//DEBUG::
-              //Do not use executeQuery.
-              stz.executeUpdate(alter_table);
-              //var sql = "ALTER TABLE MyDB.myTable ADD " + columnName + " INTEGER(30)";
+        table = "table"+x
+        var createTable = con.createStatement ()
+             createTable.executeUpdate ("CREATE TABLE " + table
+                 + "(id INTEGER )")
             }
-
-            var sample="( ?";
-            //for every added column, insert into random data.
-            for(b<-1 to col)
-            {
-              sample+=", ?";
-            }
-            sample+=" )";
-            //System.out.println(sample);//DEBUG::
-
-                //Fill with data. Randomly choose data from a list or dictionary.
-                //create the Query string to insert random data.
-                var str1 = "INSERT INTO "
-                var str2 = table;
-                var strr = str1.concat(str2);//get the String Query <INSERT INTO TABLE VALUES>
-                System.out.println(strr);//// DEBUG:
-                //var samples = "(?)"
-                 var query = strr +
-                                   " VALUES " + sample;
-
-                                    System.out.println(query);//// DEBUG:
-                                  //  var st = con.prepareStatement("INSERT INTO table16 VALUES (?)");
-                                  var st = con.prepareStatement(query);
-                // var p=con.prepareStatement(st);
-                 //Increment here, for values 1,2..
-                 st.setInt(1,random_Int);
-                 var d = 0;
-                 for(d<-1 to col)
-                 {
-
-                   st.setString(1 + d, "random_String");
-                   System.out.println(2 + d);
-
-                 }
-                 var rs=st.executeUpdate();
-
-                 //START DEBUGGING FOR getPrimaryKeys
-                 //Work with Metadata.
-
-          //get pks and then randomly pick a new one.
-          var pk_change = con.createStatement();
-          var pk = 0;
-          var alter_pk = "Alter table table2 Add primary key (id)";
-          stat.executeUpdate(alter_pk);
-
-          var PK = databaseMetaData.getPrimaryKeys(null,null, table);
-          System.out.println("------------PRIMARY KEYS-------------");
-          while(PK.next())
-          {
-            System.out.println(PK.getString("COLUMN_NAME") + "===" + PK.getString("PK_NAME"));
           }
-          //Alter datatype.
-          //statement.executeUpdate("ALTER TABLE table_name ALTER COLUMN column_name datatype");
 
 
+   def add_columns{
+
+     for(x <- 1 to a){
+
+      col = random_columns.nextInt(9)
+      colArray = colArray :+ col
+      //System.out.println(col)
+
+      table = "table"+x
+
+         var s1 = "ALTER TABLE "
+         var s2 = table
+         var st = s1.concat(s2)
+         var s3 = " add "
+         var st2 = st.concat(s3)
+         var stz = con.createStatement()
+
+         for(b <- 1 to col) {
 
 
-    }
+         var columnName = "Column" +b
+         var alter_table = st2 + columnName + " varchar(30)"
+         stz.executeUpdate(alter_table)
 
 
+          }
+        }
+      }
 
-  }
+    def add_data{
+      counter = 0//set an upper limit for tables, check it through the vector.
 
-  "one" -> "end" :=init_connect
+        for(x <- colArray) {
+
+          var sample="( ?";//named prepared_statement for data.
+          counter+=1
+
+            for(b<-1 to x){
+
+              sample+=", ?";
+              }
+              sample+=" )";//get the amount of data to be inserted.
+              //System.out.println(sample)
+              //System.out.println("stop")
+
+              var str1 = "INSERT INTO "
+              table = "table"+counter
+              var str2 = table
+              var strr = str1.concat(str2)//get the String Query <INSERT INTO TABLE VALUES>
+              System.out.println(strr)//// DEBUG:
+              var query = strr +
+                       " VALUES " + sample
+
+                        System.out.println(query)//// DEBUG:
+                      //  var st = con.prepareStatement("INSERT INTO table16 VALUES (?)");
+                      var st = con.prepareStatement(query)
+                      // var p=con.prepareStatement(st);
+                      st.setInt(1,331)//Get random integer/string.
+                      var d = 0
+                      for(d<-1 to x){
+
+                          st.setString(1 + d, "Hello World.")//get random integer/strin.
+                          System.out.println(2 + d)
+
+                        }
+
+                        var rs=st.executeUpdate()
+
+
+  //create named_prepared_statement
+                }
+
+              }
+              //TODO: Add trans. for PKS, FKS, ChangeDataType, Joins
+  "one" -> "two" :=create_table
+  "two" -> "three" :=add_columns
+  "three" -> "four" :=add_data
 }
