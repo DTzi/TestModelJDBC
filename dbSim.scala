5f29e63c983b1cea99ff4d670f782c62ee802d1a
Dimitris Tziatzios <dtzi@kth.se>
2018-11-18 20:17:37 +0100

    Updated Test Functions

--- dbSim.scala
+++ dbSim.scala
@@ -5,7 +5,7 @@ import scala.collection.mutable.HashSet
 
 
 class dbSim{
-  var myarray = Array.ofDim[Any](5,4)
+  var myarray = Array.ofDim[Any](5,10)
   val A = List("String1", "String2", "String3",
     "String4")//Using a small list for debugging.
   var trackTables = false//tracks tables that already created.
@@ -14,7 +14,7 @@ class dbSim{
   def createTable(){
     //First check if table already exists.
     if (trackTables == true) {
-      //throw org.postgresql.util.PSQLException: ERROR: relation "table" already exists 
+      //throw org.postgresql.util.PSQLException: ERROR: relation "table" already exists
       println("Table already exists")
     }
     //If not, initialise it.
@@ -29,10 +29,9 @@ class dbSim{
     }
   }
 
-  def returntable() :Boolean = {
+  def returntable() :Boolean ={
      //test Function.
      return trackTables
-
   }
 
   //Function to test number of Columns in the JDBC model.
@@ -43,7 +42,18 @@ class dbSim{
         myarray(i)(j) = 0
       }
     }
-  } 
+  }
+
+  //Test number of Columns.
+  def returnCols() :Int ={
+    var countCols = 0
+    for(j <- 0 to myarray(0).length-1){
+      if(myarray(0)(j) != null){
+        countCols += 1
+      }
+    }
+    return countCols
+  }
 
   def addPk(a:Int){
     if(trackPk.contains(a)){
@@ -53,6 +63,16 @@ class dbSim{
     else{
       trackPk = trackPk :+ a
       //println("PK added")
+    }
+  }
+
+  //Test PK.
+  def returnPK() :Boolean ={
+    if(trackPk.isEmpty){
+      return false
+    }
+    else{
+      return true
     }
   }
 
@@ -70,8 +90,7 @@ class dbSim{
   def addPkUtil(a:Int){
     val mySet = HashSet.empty[Any]
     for(i <- 0 to myarray.length-1){
-      println(myarray(i)(a))
-      if(mySet.contains(myarray(i)(a))) println("duplicate")  
+      if(mySet.contains(myarray(i)(a))) println("duplicate")
         mySet += myarray(i)(a)
     }
   }
@@ -80,7 +99,7 @@ class dbSim{
     for(i <- 0 to myarray.length-1){
       //Just delete everything in the given Column.
       myarray(i)(a) = 0
-    } 
+    }
   }
 
   def deleteTable(){
@@ -102,7 +121,7 @@ class dbSim{
       }
       println()
     }
-  }  
+  }
 }
 
 object dbSim{
@@ -116,17 +135,17 @@ object dbSim{
     //get params through JDBC MODEL.
     //Operations following JDBC model's transitions.
     //Create table.
-    mylist(0).createTable()
+    /*mylist(0).createTable()
     mylist(0).createTable()//fails with ERROR: relation "table" already exists
     //add 2 empty columns.
     mylist(0).addCols(2)
     //add pk to the table.
     mylist(0).addPk(1)
-    //add some data and check if there are any duplicates in the Primary Key Column.  
+    //add some data and check if there are any duplicates in the Primary Key Column.
     mylist(0).addData(2)
     //delete the third Column.
     mylist(0).deleteCol(2)
-    mylist(0).printArray()
+    mylist(0).printArray()*/
 
   }
 }
