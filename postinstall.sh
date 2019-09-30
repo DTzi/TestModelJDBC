9065a9f67b1584d1498ced93ae825b271c23a4a7
Dimitris Tziatzios <dtzi@kth.se>
2019-09-30 13:10:30 +0200

    Install Script

--- postinstall.sh
+++ postinstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 tar xvfz postgresql-10.5.tar.gz
 cd postgresql-10.5
-./configure --prefix=/Users/najinblue3/Desktop/mut/postgresql-10.5 
+./configure --prefix=/Users/Username/Desktop/mut/postgresql-10.5 
 make
 make install
 mkdir data
-/Users/najinblue3/Desktop/mut/postgresql-10.5/bin/initdb -D /Users/najinblue3/Desktop/mut/postgresql-10.5/data 
-/Users/najinblue3/Desktop/mut/postgresql-10.5/bin/pg_ctl -D /Users/najinblue3/Desktop/mut/postgresql-10.5/data -l logfile start
-/Users/najinblue3/Desktop/mut/postgresql-10.5/bin/createdb test
+/Users/Username/Desktop/mut/postgresql-10.5/bin/initdb -D /Users/Username/Desktop/mut/postgresql-10.5/data 
+/Users/Username/Desktop/mut/postgresql-10.5/bin/pg_ctl -D /Users/Username/Desktop/mut/postgresql-10.5/data -l logfile start
+/Users/Username/Desktop/mut/postgresql-10.5/bin/createdb test
